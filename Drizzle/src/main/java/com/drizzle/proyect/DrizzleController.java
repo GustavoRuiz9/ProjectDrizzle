ba9fc62758ea876a957237e2531e104b7aab315a
usuario <usuario@usuario-PC>
2016-09-11 23:11:52 -0500

    proyecto drizzle

---
+++ Drizzle/src/main/java/com/drizzle/proyect/DrizzleController.java
@@ -0,0 +1,18 @@
+package com.drizzle.proyect;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DrizzleController {
+	
+	@RequestMapping("index")
+	public ModelAndView redireccionPrincipal(){
+		ModelAndView Mv = new ModelAndView();
+		Mv.setViewName("Index");
+		Mv.addObject("Mensaje","Hola soy mario");
+		return Mv;
+	}
+
+}
